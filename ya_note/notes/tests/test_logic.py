8b4c880009b5069657868c919edab57fb73c23d9
wicsion <goldin301@gmail.com>
2025-03-20 18:12:10 +0300

    changes

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,9 +1,9 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-
+from django.utils.text import slugify
 from notes.models import Note
-from .fixtures import BaseTestSetUp
+from .test_fixtures import BaseTestSetUp
 
 User = get_user_model()
 
@@ -29,16 +29,42 @@ class TestNoteCreate(BaseTestSetUp):
         self.assertEqual(note.text, self.form_data['text'])
         self.assertEqual(note.author, self.user)
         self.assertEqual(note.title, self.form_data['title'])
+        self.assertEqual(note.slug, slugify(self.form_data['title']))
+
+    def test_slug_unique(self):
+        """Невозможно создать две заметки с одинаковым slug."""
+        # Создаём первую заметку
+        Note.objects.create(
+            title='Title 1',
+            text='Text 1',
+            slug='unique-slug',
+            author=self.user
+        )
+
+        note_2 = Note(
+            title='Title 2',
+            text='Text 2',
+            slug='unique-slug',
+            author=self.user
+        )
+        with self.assertRaises(ValueError):
+            note_2.full_clean()
+
+    def test_slug_auto_generation(self):
+        """Если не указан slug, он генерируется автоматически."""
+        note = Note.objects.create(
+            title='Auto generated slug',
+            text='Text with auto slug',
+            author=self.user
+        )
+        self.assertEqual(note.slug, slugify(note.title))
 
 
 class TestNoteEditDelete(BaseTestSetUp):
     """Класс теста редактирования заметки."""
 
-    NOTE_TEXT = 'Текст комментария'
-    NEW_NOTE_TEXT = 'Обновлённый комментарий'
-
     def test_author_can_delete_note(self):
-        """Тест, что автор заметки может ее удалить."""
+        """Тест, что автор заметки может её удалить."""
         before_count = Note.objects.count()
         response = self.author_client.delete(self.urls['delete'])
         self.assertRedirects(response, self.urls['success'])
@@ -46,7 +72,7 @@ class TestNoteEditDelete(BaseTestSetUp):
         self.assertEqual(after_count, before_count - 1)
 
     def test_user_cant_del_note(self):
-        """тест, что читатель не может удалить заметку."""
+        """Тест, что читатель не может удалить заметку."""
         before_count = Note.objects.count()
         response = self.user_client.delete(self.urls['delete'])
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
@@ -54,24 +80,22 @@ class TestNoteEditDelete(BaseTestSetUp):
         self.assertEqual(after_count, before_count)
 
     def test_author_can_edit_note(self):
-        """тест на редактирование заметки."""
-        note_id = self.notes.id
-        self.form_data['text'] = self.NEW_NOTE_TEXT
-        response = self.author_client.post(
-            self.urls['edit'], data=self.form_data
-        )
-        updated_note = Note.objects.get(id=note_id)
+        """Тест на редактирование заметки."""
+        self.form_data['text'] = 'Updated text'
+        response = self.author_client.post(self.urls['edit'],
+                                           data=self.form_data)
+        updated_note = Note.objects.get(id=self.notes.id)
         self.assertRedirects(response, self.urls['success'])
         self.assertEqual(updated_note.text, self.form_data['text'])
         self.assertEqual(updated_note.author, self.notes.author)
         self.assertEqual(updated_note.title, self.form_data['title'])
+        self.assertEqual(updated_note.slug, self.notes.slug)
 
     def test_user_cant_edit_comment_of_another_user(self):
-        """Тест, что юзер не может редактировать другие заметки."""
+        """Тест, что юзер не может редактировать чужую заметку."""
         note_id = self.notes.id
-        response = self.user_client.post(
-            self.urls['edit'], data=self.form_data
-        )
+        response = self.user_client.post(self.urls['edit'],
+                                         data=self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         updated_note = Note.objects.get(id=note_id)
         self.assertEqual(updated_note.text, self.notes.text)
